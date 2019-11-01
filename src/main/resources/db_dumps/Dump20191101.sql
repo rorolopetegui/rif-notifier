8e22e8487161a5fc653729e78e6857d5dde3fbfd
Rodrigo Lopetegui <rorolopetegui@gmail.com>
2019-11-01 20:43:05 -0300

    Last changes in DB

--- src/main/resources/db_dumps/Dump20191101.sql
+++ src/main/resources/db_dumps/Dump20191101.sql
@@ -80,10 +80,9 @@ DROP TABLE IF EXISTS `notification_counter`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `notification_counter` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
   `subscription_id` int(11) NOT NULL,
   `counter` int(11) NOT NULL,
-  PRIMARY KEY (`id`,`subscription_id`)
+  PRIMARY KEY (`subscription_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -161,11 +160,10 @@ DROP TABLE IF EXISTS `subscription_type`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `subscription_type` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `subscription_type` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `notification_counter` int(11) NOT NULL,
-  PRIMARY KEY (`id`,`subscription_type`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`,)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
